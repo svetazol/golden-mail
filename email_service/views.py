1012cf5c9b0cb84a9f02a9ed633cf536becf72b5
svetazol <svests@yandex.ru>
2016-09-13 19:54:40 +0300

    view only current user's "email requests"

--- email_service/views.py
+++ email_service/views.py
@@ -1,7 +1,10 @@
 from time import sleep
 
 from django.http import Http404
+from rest_framework import generics
+from rest_framework import mixins
 from rest_framework import permissions
+from rest_framework.generics import get_object_or_404
 from rest_framework_extensions.cache.decorators import cache_response
 from email_service.models import EmailRequest
 from email_service.permissions import IsOwnerOrReadOnly
@@ -25,16 +28,27 @@ class EmailRequestDetail(APIView):
         return Response(serializer.data)
 
 
-class EmailRequestList(APIView):
+class EmailRequestList(mixins.ListModelMixin,
+                       generics.GenericAPIView):
+    queryset = EmailRequest.objects.all()
+    serializer_class = EmailRequestSerializer
+
     # fixme: IsOwnerOrReadOnly, check_object_permissions is not called for APIView (but for GenericAPIView), add filter by user to get method?
     permission_classes = (permissions.IsAuthenticated, IsOwnerOrReadOnly,)
 
+    # IsOwnerOrReadOnly by such filter?
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user)
+
     @cache_response(cache_errors=False, timeout=15 * 60)
-    def get(self, request):
-        email_requests = EmailRequest.objects.all()
-        sleep(20)
-        serializer = EmailRequestSerializer(email_requests, many=True)
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    # def get(self, request):
+    #     email_requests = EmailRequest.objects.all()
+    #     sleep(20)
+    #     serializer = EmailRequestSerializer(email_requests, many=True)
+    #     return Response(serializer.data)
 
     def post(self, request):
         data = request.data.copy()
@@ -44,6 +58,7 @@ class EmailRequestList(APIView):
             email_request_obj = serializer.save(status='ожидает')
             send_email.apply_async((email_request_obj.message, email_request_obj.email, email_request_obj.pk),
                                    eta=email_request_obj.sending_dttm)
+            # cache.delete('...')
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
